5cf1ca85da8f6bf9aa98600ec098381ade9dde21
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2026-02-11 22:38:58 -0300

    feat(repository): implementa interfaces de persistencias das entidades Documento e Comentario

---
+++ src/main/java/br/com/testrmh/repository/DocumentoRepository.java
@@ -0,0 +1,9 @@
+package br.com.testrmh.repository;
+
+import br.com.testrmh.model.Documento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentoRepository extends JpaRepository<Documento, Long> {
+}
