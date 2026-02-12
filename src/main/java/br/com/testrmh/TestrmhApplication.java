1bb5cc0c1b531bfe8992a5ae1c212f41c53d04fa
Guilherme Rodrigues <Guilhermesmx@gmail.com>
2026-02-11 21:57:35 -0300

    Commit inicial

---
+++ src/main/java/br/com/testrmh/TestrmhApplication.java
@@ -0,0 +1,13 @@
+package br.com.testrmh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestrmhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestrmhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/testrmh/TestrmhApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.testrmh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestrmhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
